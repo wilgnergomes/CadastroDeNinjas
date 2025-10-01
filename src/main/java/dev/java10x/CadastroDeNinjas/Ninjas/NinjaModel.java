978dfb240b4784eae1d1a49aca5efbb5e1e2011c
Wilgner Gomes <wilgnersgomes@gmail.com>
2025-10-01 17:02:07 -0300

    fix(ninja): define nomes de colunas e restrições com @Column

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,9 +18,16 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(name = "idade")
     private int idade;
 
     // @ManyToOne: um ninja tem uma única missão
